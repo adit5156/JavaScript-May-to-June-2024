8fe3ffb1d9bb63c5ba2fc5e86b0e98bacdb2f30f
adit5156 <adityasinghmpc2@gmail.com>
2024-05-29 20:20:42 +0530

    updated 29-05-2024

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // Treats all JS Code as newer version
+
+// alert(3 + 3);
+
+console.log(3 + 3);
+console.log("Hitesh");
+
+let name = "Hitesh";
+let age = 19;
+let isLoggedIn = true;
+let state;
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isLoggedIn));
+console.log(typeof(state));
+console.log(typeof null);
+
+// Primitive Data Types
+// number => 2 to the power 53
+// string => "" or ''
+// boolean => true or false
+// undefined
+// null => standalone value
+// symbol => unique
+// bigInt 
+
+// Reference / Non Primitive Data Types
+// arrays
+// functions
+// objects
