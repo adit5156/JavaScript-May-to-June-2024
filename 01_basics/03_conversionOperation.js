34215a5b88458341272fc031bd7e7de0ef0b9b29
adit5156 <adityasinghmpc2@gmail.com>
2024-05-29 22:50:15 +0530

    updated 29-05-2024

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+// let score = "33abc";
+// let score = null;
+// let score = undefined;
+// let score = true;
+// let score = "Aditya Singh";
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// undefined => NaN
+// null => 0
+
+
+// let isLoggedIn = "true";
+// console.log(typeof isLoggedIn);
+
+// let valueInBoolean = Boolean(isLoggedIn);
+// console.log(typeof valueInBoolean);
+// console.log(valueInBoolean);
+
+// 1 => true
+// "adityaistheonlyking" => true
+// 53 => true
+// 0 => false
+// NaN => false
+// undefined => false
+// null => false
+// "" => false
+
+
+let someNumber = 33;
+console.log(someNumber);
+console.log(typeof someNumber);
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(someNumber);
