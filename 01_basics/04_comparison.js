1b0037408935e61aea4a5a26fcb1b17cc038e1f8
adit5156 <adityasinghmpc2@gmail.com>
2024-05-30 08:30:53 +0530

    updated 30-05-2024

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+// Strict Check ===
+
+console.log("2" == 2);
+console.log("2" === 2);
