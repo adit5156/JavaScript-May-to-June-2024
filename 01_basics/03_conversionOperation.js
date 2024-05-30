da221873e8b2def40ffbba9594162409a52cd9b1
adit5156 <adityasinghmpc2@gmail.com>
2024-05-30 08:17:05 +0530

    updated 30-05-2024

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,7 +17,6 @@
 // undefined => NaN
 // null => 0
 
-
 // let isLoggedIn = "true";
 // console.log(typeof isLoggedIn);
 
@@ -34,11 +33,48 @@
 // null => false
 // "" => false
 
+// let someNumber = 33;
+// console.log(someNumber);
+// console.log(typeof someNumber);
 
-let someNumber = 33;
-console.log(someNumber);
-console.log(typeof someNumber);
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+// console.log(someNumber);
 
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(someNumber);
+// *********************************** Operations **********************************
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(12 - 2);
+// console.log(2 * 4);
+// console.log(32 / 4);
+// console.log(33 % 4);
+// console.log(4 ** 4);
+
+// let str1 = "Hello ";
+// let str2 = "Aditya!";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log( 1 + "2");
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(false);
+// console.log(+false);
+// console.log(+"");
+
+let gameCounter = 100;
+// ++gameCounter; // Prefix-Increment 
+// gameCounter++; // Postfix-Increment 
+// --gameCounter; // Prefix-Decrement 
+// gameCounter--; // Postfix-Decrement
+console.log(gameCounter);
