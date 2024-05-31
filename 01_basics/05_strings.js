f261ace863d883b4369f4fcd28d14c0b992f48ad
adit5156 <adityasinghmpc2@gmail.com>
2024-05-31 10:21:36 +0530

    updated 31-05-2024

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,32 @@
+// const name = "Aditya";
+// const repoCount = 50;
+
+// console.log(name + " " + repoCount + " is the GitHub repo count!");
+// console.log(`Hello, my name is ${name} and my GitHub repo count is ${repoCount}`);
+
+const gameName = new String("aditya-victus-com");
+// console.log(gameName);
+
+// console.log(gameName.length);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const newString2 = gameName.slice(2, 4);
+console.log(newString2);
+
+console.log(gameName.split('-'));
+
+// const trimString = "        aditya             ";
+// console.log(trimString);
+// console.log(trimString.trim());
+
+// const url = "https://aditya.com/aditya%20singh";
+// console.log(url.replace("%20", "-"));
+
+// console.log(url.includes("aditya"));
