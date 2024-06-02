688b3dafe336066460bee18f928bc2f9c1ff3012
adit5156 <adityasinghmpc2@gmail.com>
2024-06-02 22:27:15 +0530

    updated 2-06-2024

---
+++ 02_basics/objectDestructuring-and-JSON.js
@@ -0,0 +1,41 @@
+const course = {
+    courseName: "JavaScript in One Go",
+    coursePrice: "999",
+    courseInstructor: "Aditya"
+};
+
+// console.log(course.courseInstructor);
+
+
+// Object Destructuring
+
+// Syntax
+
+// let/const {object_key : short_name} = object;
+
+
+const {courseInstructor: instructor} = course;
+console.log(instructor);
+
+
+// JSON => JavaScript Object Notation
+
+/*
+{
+    "key" : "value"
+}
+*/
+
+/*
+{
+    "name": "Aditya",
+    "courseName": "JavaScript in One Go",
+    "price": "free"
+}
+*/
+
+[
+    {},
+    {},
+    {}
+]
