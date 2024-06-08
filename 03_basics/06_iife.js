739bb1ce23453211095f80f1ab0323d8dece0699
adit5156 <adityasinghmpc2@gmail.com>
2024-06-08 22:06:36 +0530

    updated 8-06-2024

---
+++ 03_basics/06_iife.js
@@ -0,0 +1,33 @@
+// Immediately Invoked Function Expression (IIFE)
+
+// function chai() {
+//     console.log(`DB Connected`);
+// }
+// chai();
+
+// To prevent the global object clutter we use IIFE 
+
+// () Function Declaration () Function Execution / Function Call
+
+// ; is used after invoking a function so to break the flow of execution 
+
+(function chai() {
+    // Named IIFE
+    console.log(`DB Connected`);
+}) ();
+
+
+( () => {
+    // Unnamed IIFE
+    console.log(`DB2 Connected Successfully`);
+}) ();
+
+((name) => {
+    // Unnamed IIFE
+    console.log(`${name}, Welcome to the database!`);
+}) ("Aditya");
+
+(function unnamedIIFE() {
+    // Named IIFE
+    console.log("Welcome to Unnamed IIFE");
+}) ();
