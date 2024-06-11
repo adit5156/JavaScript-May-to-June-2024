e35e3e8f61f0472299b9ccf1dd8a99d9b4fb88e6
adit5156 <adityasinghmpc2@gmail.com>
2024-06-11 09:04:56 +0530

    updated 11-06-2024

---
+++ 04_control-flow/02_switch.js
@@ -0,0 +1,63 @@
+const month = 9;
+
+switch(month) {
+    case 1: 
+        console.log("January");
+        break;
+    case 2: 
+        console.log("February");
+        break;
+    case 3: 
+        console.log("March");
+        break;
+    case 4: 
+        console.log("April");
+        break;
+    case 5: 
+        console.log("May");
+        break;
+    case 6: 
+        console.log("June");
+        break;
+    case 7: 
+        console.log("July");
+        break;
+    case 8: 
+        console.log("August");
+        break;
+    case 9: 
+        console.log("September");
+        break;
+    case 10: 
+        console.log("October");
+        break;
+    case 11: 
+        console.log("November");
+        break;
+    case 12: 
+        console.log("December");
+        break;
+    default: 
+        console.log("Enter month correctly!");
+        break;
+}
+
+const months = "april";
+
+switch(months) {
+    case "january":
+        console.log("January");
+        break;
+    case "february":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+    default:
+        console.log("Enter month carefully!");
+        break;
+}
