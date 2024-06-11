b52cc15e62dd9ceb4158be76781ca0dcdf4adb2d
adit5156 <adityasinghmpc2@gmail.com>
2024-06-11 19:21:16 +0530

    updated 11-06-2024

--- 05_iterations/01_for-loops-break-continue.js
+++ 05_iterations/01_for-loops-break-continue.js
@@ -1,5 +1,12 @@
 // for loop
 
+// Syntax: 
+
+/*for(initialisation; condition; increment / decrement) {
+    execution
+}*/
+
+
 // for(let i = 1; i <= 10; i++) {
 //     if(i === 5) {
 //         console.log("5 is the best number!");
---
+++ 05_iterations/02_while-and-do-while-loops.js
@@ -0,0 +1,59 @@
+// while loop
+
+// Syntax:
+/* 
+    initialisation
+    while(condtion) {
+        execution
+        increment / decrement
+    }
+
+*/
+
+
+// let i = 0;
+// while (i <= 10) {
+//   console.log("Value of i is: ", i);
+//   i++;
+// }
+
+
+
+// let i = 0;
+// while (i <= 10) {
+//   console.log("Value of i is: ", i);
+//   i = i + 2;
+// }
+
+
+
+let myArray = ["flash", "batman", "superman"];
+// let i = 0;
+// while (i < myArray.length) {
+//   console.log(`Value at index ${i} = ${myArray[i]}`);
+//   i++;
+// }
+
+
+
+// let i = 0;
+// while(i < myArray.length) {
+//     console.log(`Value at index ${i} = ${myArray[i]}`);
+//     i = i + 2;
+// }
+
+
+
+// let score = 1;
+// do {
+//     console.log("Score: ", score);
+//     score = score + 1;
+// } while(score <= 10);
+
+
+
+let score = 11;
+do {
+    console.log("Score: ", score);
+    score = score + 1;
+} while(score <= 10);
