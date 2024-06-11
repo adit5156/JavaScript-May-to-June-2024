68e03ca09ddf206b019b50da1d840e11955f4c71
adit5156 <adityasinghmpc2@gmail.com>
2024-06-11 09:05:14 +0530

    updated 11-06-2024

---
+++ 04_control-flow/03_truthy-falsy.js
@@ -0,0 +1,34 @@
+// Falsy Values => 0 , NaN , undefined , null , "" , false , -0, BigInt(0n)
+// Truthy Values => "0" , "false" , " " , [] , {} , function () {}
+// Anything except the falsy values are the truthy values
+
+// const userEmail = "aditya@ai";
+// if(userEmail) {
+//     console.log("Got user email");
+// }
+// else {
+//     console.log("Don't have user email");
+// }
+
+
+// const isArrayEmpty = [];
+// if(isArrayEmpty.length === 0) {
+//     console.log("Yes, Array is empty");
+// }
+// else {
+//     console.log("No, Array is not empty");
+// }
+
+
+// const isObjectEmpty = {};
+// if(Object.keys(isObjectEmpty).length === 0) {
+//     console.log("Yes, Object is empty");
+// }
+// else {
+//     console.log("No, Object is not empty");
+// }
+
+
+// false == 0       // Output => true
+// false == ''      // Output => true
+// 0 == ''      // Output => true
