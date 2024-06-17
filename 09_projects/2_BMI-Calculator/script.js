101cb722a6c47223ecc34e5480da299aa589bc19
adit5156 <adityasinghmpc2@gmail.com>
2024-06-17 11:47:28 +0530

    updated 17-06-2024

---
+++ 09_projects/2_BMI-Calculator/script.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    const height = (document.querySelector('#height').value) / 100;
+    const weight = document.querySelector('#weight').value;
+    const bmi = ((weight) / (height ** 2)).toFixed(2);
+    
+    const result = document.querySelector('#results')
+    result.innerText = `${bmi} = `;
+
+    if(bmi < 18.6) {
+        result.appendChild(document.createTextNode('Under Weight'));
+    }
+    else if(bmi >= 18.6 && bmi < 24.9) {
+        result.appendChild(document.createTextNode('Normal Range'));
+    }
+    else if(bmi > 24.9) {
+        result.appendChild(document.createTextNode("Overweight"));
+    }
+});
