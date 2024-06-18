2ae67d86128e643643a14ec140727065b6120cf2
adit5156 <adityasinghmpc2@gmail.com>
2024-06-18 19:07:00 +0530

    updated 18-06-2024

--- 09_projects/2_BMI-Calculator/script.js
+++ 09_projects/2_BMI-Calculator/script.js
@@ -2,8 +2,8 @@ const form = document.querySelector('form');
 
 form.addEventListener('submit', (e)=> {
     e.preventDefault();
-    const height = (document.querySelector('#height').value) / 100;
-    const weight = document.querySelector('#weight').value;
+    const height = parseInt(document.querySelector('#height').value) / 100;
+    const weight = parseInt(document.querySelector('#weight').value);
     const bmi = ((weight) / (height ** 2)).toFixed(2);
     
     const result = document.querySelector('#results')
--- 09_projects/3_Digital-Clock/script.js
+++ 09_projects/3_Digital-Clock/script.js
@@ -2,5 +2,6 @@ const clock = document.querySelector('#clock');
 
 setInterval(() => {
     let date = new Date();
+    // console.log(date.toLocaleTimeString());
     clock.innerHTML = date.toLocaleTimeString();
 }, 1000);
