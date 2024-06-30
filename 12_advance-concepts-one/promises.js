24a6bd2d88a84aeb9fb885ad07505e125d9ab14f
adit5156 <adityasinghmpc2@gmail.com>
2024-06-30 14:00:56 +0530

    updated 30-06-2024

--- 12_advance-concepts-one/promises.js
+++ 12_advance-concepts-one/promises.js
@@ -127,28 +127,29 @@ async function promiseSixConsumption() {
 promiseSixConsumption();
 
 
-// *************************************************************************
+// ******************************************************************
 
-// async function getAllUsers(link) {
-//     try {
-//         const response = await fetch(link);
-//         const data = await response.json();
-//         console.log(data);
-//     }
-//     catch(error) {
-//         console.log("Error: ", error);
-//     }
+// async function getAllUsers() {
+//   try{
+//     const response = await fetch("https://api.github.com/users/adit5156");
+//     const data = await response.json();
+//     console.log(data);
+//   }
+//   catch(error) {
+//     console.log("Error: ", error);
+//   }
 // }
-// getAllUsers('https://api.github.com/users/adit5156');
+// getAllUsers();
 
+// ******************************************************************
 
-fetch('https://api.github.com/users/adit5156')
+fetch("https://api.github.com/users/adit5156")
 .then(function(response) {
-    return response.json();
+  return response.json();
 })
 .then(function(data) {
-    console.log(data);
+  console.log(data);
 })
 .catch(function(error) {
-    console.log("Error: ", error);
+  console.log("Error: ", error);
 });
